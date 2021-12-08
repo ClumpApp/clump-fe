eeffeec3808ef397ade148bb52f559792f9d952c
sdoganata <81318317+sdoganata@users.noreply.github.com>
2021-12-09 01:06:22 +0300

    const file created
    color code constants added

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+//color codes
+const lightOrange = Color(0xfffbc02d);
+const darkOrange = Color(0xfff57f12);
+const darkGrey = Color(0xff323232);
+const darkPurple = Color(0xff241e4e);
+const softerPurple = Color(0xff441f7e);
+const magenta = Color(0xffc90076);
+const turquoise = Color(0xff067084);
+
+//color places
+const notifTextColor = darkOrange;
+const usernameTextColor = turquoise;
+const headerTextColor = softerPurple;
+const backgroundColor = darkGrey;
+const tagTextColor = magenta;
+const themeColor = darkPurple;
