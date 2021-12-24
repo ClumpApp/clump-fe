fa5c5bc9ba28365bb0f7e5a2e40967edfe2f267e
sdoganata <81318317+sdoganata@users.noreply.github.com>
2021-12-24 14:53:53 +0300

    interests main

--- lib/pages/signup_main.dart
+++ lib/pages/signup_main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SignUpMain(),
+      home: const SignUpMain(),
     );
   }
 }
@@ -55,63 +55,72 @@ class _SignUpMain extends State<SignUpMain> {
 //    return MediaQuery(
 //        data: MediaQueryData(),
 //        child: MaterialApp(home: Builder(builder: (BuildContext context) {
-    return
-//    Container(
-//      decoration: const BoxDecoration(
-//        image: DecorationImage(
-//          image: AssetImage("assets/images/backgrounds/Clump BG_interests.png"),
-//          fit: BoxFit.cover,
-//        ),
-//      ),
-//      child:
-        Scaffold(
-      body: Wrap(children: <Widget>[
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Checkbox(
-                value: selectAll,
-                onChanged: (bool? value) {
-                  setState(() {
-                    selectAll = value!;
-                    ints.forEach((element) {
-                      element.selected = value;
-                    });
-                  });
-                }),
-            GridView.builder(
-              physics: ScrollPhysics(),
-              shrinkWrap: true,
-              itemBuilder: (ctx, index) {
-                return prepareList(index, screenSize);
-              },
-              gridDelegate:
-                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
-              itemCount: ints.length,
-            ),
-          ],
-        ),
-        Container(
-          child: MaterialButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SignUpSub()),
-              );
-            },
-            elevation: 0,
-            color: turquoise,
-            child: const Text(
-              'Cool!',
-              style: TextStyle(
-                fontSize: 16,
-                color: lightOrange,
-              ),
-            ),
+    return Container(
+        padding: EdgeInsets.only(top: screenSize.height / 4),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image:
+                AssetImage("assets/images/backgrounds/Clump BG_interests1.png"),
+            fit: BoxFit.cover,
           ),
         ),
-      ]),
-    );
+        child: Scaffold(
+            backgroundColor: blackpurple,
+            body: SingleChildScrollView(
+                child: Container(
+              //padding: EdgeInsets.only(top: screenSize.height / 10),
+              width: double.infinity,
+              child: Wrap(children: <Widget>[
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    //Checkbox(
+                    //    value: selectAll,
+                    //    onChanged: (bool? value) {
+                    //      setState(() {
+                    //        selectAll = value!;
+                    //        ints.forEach((element) {
+                    //          element.selected = value;
+                    //        });
+                    //      });
+                    //    }),
+                    GridView.builder(
+                      physics: ScrollPhysics(),
+                      shrinkWrap: true,
+                      itemBuilder: (ctx, index) {
+                        return prepareList(index, screenSize);
+                      },
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 4),
+                      itemCount: ints.length,
+                    ),
+                  ],
+                ),
+                Container(
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SignUpSub()),
+                      );
+                    },
+                    elevation: 0,
+                    color: Colors.transparent,
+                    height: screenSize.height / 5,
+                    child: const Center(
+                      child: Text(
+                        'Cool!',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
+            ))));
     //})));
   }
 
@@ -122,6 +131,8 @@ class _SignUpMain extends State<SignUpMain> {
       child: Hero(
         tag: "text$k",
         child: Material(
+          elevation: 0,
+          color: Colors.transparent,
           child: InkWell(
             onTap: () {},
             child: Container(
